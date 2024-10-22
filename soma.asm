f552d15536994af7e699634a0b261970f4e41c8d
Carlos <carlosabcj1@gmail.com>
2024-10-22 14:18:29 -0300

    correção do codigo

--- soma.asm
+++ soma.asm
@@ -2,7 +2,7 @@ section .data
     num1 dq 10
     num2 dq 20
     result dq 0
-    msg db 'O Resultado é'
+    msg db 'O Resultado é',10
 
 section .text
     global _start
@@ -11,8 +11,9 @@ _start:
     mov rax,[num1]
     add rax,[num2]
     mov [result],rax
-    mov rsi message
+    mov rsi,msg
     syscall       
+    
     mov rax,60
     xor rdi,rdi
     syscall
